6675008e18c38ea7b2142fa68f608d27ee767623
Siphamandla <mr.smehlomakulu@gmail.com>
2020-08-23 19:36:04 +0200

    Add Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navigation from './components/navigation/navigation';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Navigation />
     </div>
   );
 }
