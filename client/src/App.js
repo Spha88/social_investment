1374a8fafdc5688ce933dcb5baff6ba5420b1f17
Siphamandla <mr.smehlomakulu@gmail.com>
2020-08-25 15:18:35 +0200

    Create signup form and page

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import Loans from './components/pages/loans/Loans';
 import LoanApplication from './components/pages/loanApplication/LoanApplication';
 import Profile from './components/pages/Profile/Profile';
 import Login from './components/pages/Login/Login';
+import Signup from './components/pages/Signup/Signup';
 
 function App() {
   return (
     <React.Fragment>
       <Navigation />
       <Switch>
+        <Route path="/signup" component={Signup} />
         <Route path="/login" component={Login} />
         <Route path="/loans" component={Loans} />
         <Route path="/apply" component={LoanApplication} />
